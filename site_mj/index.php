4e83ab96b95ae186f29424ff864d33a26038d9a5
Anton <a-pavlyuchenkov@bk.ru>
2022-10-23 19:07:08 +0000

    Deleted unnesessery files

--- site_mj/index.php
+++
@@ -1,1 +0,0 @@
-<?define("WIZARD_DEFAULT_SITE_ID", "mj");require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/classes/general/wizard.php");$wizard = new CWizard("bitrix:corp_furniture");$wizard->Install();require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/epilog_after.php");?>
