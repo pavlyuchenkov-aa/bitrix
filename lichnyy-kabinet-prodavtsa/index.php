94bf5c3da88f0eba462f58e21bb25ef7e44ee4c4
Anton <a-pavlyuchenkov@bk.ru>
2022-10-30 06:46:37 +0000

    Added layouts for customers' and sallers' personal accounts

--- lichnyy-kabinet-prodavtsa/index.php
+++ lichnyy-kabinet-prodavtsa/index.php
@@ -1,4 +1,17 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Личный кабинет продавца");
-?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Личный кабинет продавца");
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:main.profile",
+	"",
+	Array(
+		"CHECK_RIGHTS" => "N",
+		"SEND_INFO" => "N",
+		"SET_TITLE" => "N",
+		"USER_PROPERTY" => array(),
+		"USER_PROPERTY_NAME" => ""
+	)
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
